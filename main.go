ab4e9b53ff0f8a3eb9f738559e308b05736a561b
AJ-Brown-InTech <ajalantbrown@gmail.com>
2025-03-08 16:06:02 -0600

    fix: problem 1

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
     "log"
-    "github.com/AJ-Brown-InTech/LeetCode/twosum" 
+  "LeetCode/twosum"
 )
 
 func main() {
-    if err := twosum.Setup(); len(err) > 0 {
-        log.Println(err)
+    if test, _ := twosum.Setup(); len(test) > 0 {
+        log.Println(test)
         return
     }
 }    
